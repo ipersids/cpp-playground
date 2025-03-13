ecd63a2ab5dc6faef68f89815ea742eed3239b08
Julia Llama <julia.persidskaia@gmail.com>
2025-03-13 13:15:56 +0200

    Add module 01 exrcise 02

---
+++ module-01/ex02/main.cpp
@@ -0,0 +1,29 @@
+/**
+ * @file main.cpp
+ * @author Julia Persidskaia
+ * @date 2025-03-13
+ */
+
+#include <iostream>
+
+int main(void) {
+  std::string str = "HI THIS IS BRAIN";
+  std::string* stringPTR = &str;
+  std::string& stringREF = str;
+
+  std::cout << "The memory address of the string variable:\t"
+            << &str
+            << "\nThe memory address held by stringPTR:\t\t"
+            << stringPTR
+            << "\nThe memory address held by stringREF:\t\t"
+            << &stringREF
+            << std::endl;
+  
+  std::cout << "\nThe value of the string variable:\t\t"
+            << str
+            << "\nThe value pointed to by stringPTR:\t\t"
+            << *stringPTR
+            << "\nThe value pointed to by stringREF:\t\t"
+            << stringREF
+            << std::endl;
+}
