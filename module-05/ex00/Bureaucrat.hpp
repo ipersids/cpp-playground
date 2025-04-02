63315d68a8077d5916876fa84ee5f2933adf3a59
Julia Llama <julia.persidskaia@gmail.com>
2025-04-03 01:48:19 +0300

    Add module 05 exercise 00

---
+++ module-05/ex00/Bureaucrat.hpp
@@ -0,0 +1,62 @@
+/**
+ * @file Bureaucrat.hpp
+ * @author Julia Persidskaia (julia.persidskaia@gmail.com)
+ * @date 2025-04-03
+*/
+
+#ifndef BUREAUCRAT_EX00_HPP_
+#define BUREAUCRAT_EX00_HPP_
+
+#include <iostream>
+#include <stdexcept>
+
+#ifndef COLORS
+#define COLORS
+#define GREEN "\033[32m"
+#define BLUE "\033[34m"
+#define RED "\033[31m"
+#define MAGENTA "\033[35m"
+#define RESET "\033[0m"
+#endif  // COLORS
+
+class Bureaucrat {
+  public:
+    Bureaucrat();
+    Bureaucrat(const std::string name, int grade);
+    Bureaucrat(const Bureaucrat& other) = delete;
+    Bureaucrat& operator=(const Bureaucrat& other) = delete;
+    ~Bureaucrat();
+
+    const std::string& getName(void) const;
+    int getGrade(void) const;
+
+    class GradeTooHighException: public std::exception {
+      public:
+        GradeTooHighException();
+        GradeTooHighException(const Bureaucrat& obj);
+
+        const char* what() const noexcept override;
+
+      private:
+        const std::string _message;
+    };
+
+    class GradeTooLowException: public std::exception {
+      public:
+        GradeTooLowException();
+        GradeTooLowException(const Bureaucrat& obj);
+
+        const char* what() const noexcept override;
+
+      private:
+        const std::string _message;
+    };
+
+  private:
+    const std::string _name;
+    int _grade; // range from 1 (highest) to 150 (lowest)
+};
+
+std::ostream& operator<<(std::ostream& os, const Bureaucrat& b);
+
+#endif // BUREAUCRAT_EX00_HPP_
---
+++ module-05/ex00/main.cpp
@@ -0,0 +1,70 @@
+/**
+ * @file main.cpp
+ * @author Julia Persidskaia (julia.persidskaia@gmail.com)
+ * @date 2025-04-03
+*/
+
+#include <iostream>
+
+#include "Bureaucrat.hpp"
+
+int main(void) {
+  try {
+    std::cout << GREEN << "\n---------- TEST 1 (CORRECT GRADE) ----------\n" << RESET << std::endl;
+    Bureaucrat dr_strange;
+    Bureaucrat cp_marvel("Captain Marvel", 1);
+    Bureaucrat spider_man("Spider-Man", 150);
+    Bureaucrat hulk("Hulk", 66);
+
+    std::cout << "\n---> Call getters:" << std::endl;
+    std::cout << "-getName(): " << dr_strange.getName() << std::endl;
+    std::cout << "-getGrade(): " << dr_strange.getGrade() << std::endl;
+
+    std::cout << "\n---> Call << operator:" << std::endl;
+    std::cout << dr_strange << std::endl;
+    std::cout << cp_marvel << std::endl;
+    std::cout << spider_man << std::endl;
+    std::cout << hulk << std::endl;
+
+    std::cout << "\n---> Call destructors:" << std::endl;
+
+  } catch (const std::exception& e) {
+    std::cerr << RED << e.what() << RESET << std::endl;
+  }
+
+  try {
+    std::cout << GREEN << "\n--------- TEST 2 (TOO HIGH GRADE) ----------\n" << RESET << std::endl;
+    Bureaucrat black_widow("Black Widow", 0);
+
+    std::cout << "\n---> Call getters:" << std::endl;
+    std::cout << "-getName(): " << black_widow.getName() << std::endl;
+    std::cout << "-getGrade(): " << black_widow.getGrade() << std::endl;
+
+    std::cout << "\n---> Call << operator:" << std::endl;
+    std::cout << black_widow << std::endl;
+
+    std::cout << "\n---> Call destructors:" << std::endl;
+
+  } catch (const std::exception& e) {
+    std::cerr << RED << e.what() << RESET << std::endl;
+  }
+
+  try {
+    std::cout << GREEN << "\n--------- TEST 3 (TOO LOW GRADE) ----------\n" << RESET << std::endl;
+    Bureaucrat iron_man("Iron Man", 151);
+
+    std::cout << "\n---> Call getters:" << std::endl;
+    std::cout << "-getName(): " << iron_man.getName() << std::endl;
+    std::cout << "-getGrade(): " << iron_man.getGrade() << std::endl;
+
+    std::cout << "\n---> Call << operator:" << std::endl;
+    std::cout << iron_man << std::endl;
+
+    std::cout << "\n---> Call destructors:" << std::endl;
+
+  } catch (const std::exception& e) {
+    std::cerr << RED << e.what() << RESET << std::endl;
+  }
+
+  return 0;
+}
