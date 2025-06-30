229366ef46510df73980972003bc3f43d85a4044
Julia Llama <julia.persidskaia@gmail.com>
2025-06-30 23:01:29 +0300

    Add rank-05 ex00 solution

---
+++ rank-05/ex00/Warlock.hpp
@@ -0,0 +1,26 @@
+#ifndef WARLOCK_EX00_H
+#define WARLOCK_EX00_H
+
+#include <iostream>
+
+class Warlock {
+  public:
+    Warlock() = delete;
+    Warlock(const std::string& name, const std::string& title);
+    Warlock(const Warlock& other) = delete;
+    Warlock& operator=(const Warlock& other) = delete;
+    ~Warlock();
+
+  
+    const std::string& getName() const;
+    const std::string& getTitle() const;
+    void setTitle(const std::string& title);
+    void introduce() const;
+
+  private:
+    std::string _name;
+    std::string _title;
+};
+
+#endif // WARLOCK_EX00_H
+
